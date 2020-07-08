64e000ff99cfa48a3e8421268126fc8c6f8f0939
joearpy <joearpy87@gmail.com>
2020-07-08 15:56:00 +0200

    Calendar, Select, API connection

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,71 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <multiselect
+      v-model="select.selected"
+      :options="select.options"
+      track-by="ID"
+      label="name"
+      >
+    </multiselect>
+    {{selected}}
+    <vue-event-calendar :events="demoEvents"></vue-event-calendar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'vue-event-calendar/dist/style.css';
+import "vue-multiselect/dist/vue-multiselect.min.css";
+import Multiselect from 'vue-multiselect';
+import axios from "axios";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Multiselect
+  },
+  data () {
+    return {
+      select: {
+        selected: null,
+        options: []
+      },
+      events: [
+
+      ],
+      demoEvents: [{
+        date: '2020-07-13', // Required
+        title: 'Foo' // Required
+      }, {
+        date: '2020/07/11',
+        title: 'Bar',
+        desc: 'description',
+      }]
+    }
+  },
+  created: function() {
+    this.get_categories();
+  },
+  methods: {
+    get_categories() {
+      axios.post(process.env.VUE_APP_API_PROTOCOLL + process.env.VUE_APP_API_BASE_URL + process.env.VUE_APP_API_VERSION + "/get_categories", {})
+        .then((response) => {
+          this.select.options = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+      });
+    },
+    get_events_by_category() {
+      axios.post(process.env.VUE_APP_API_PROTOCOLL + process.env.VUE_APP_API_BASE_URL + process.env.VUE_APP_API_VERSION + "/get_events_by_category", {})
+        .then((response) => {
+          this.events = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+      });
+    }
+  },
 }
 </script>
 
@@ -23,6 +76,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import vueEventCalendar from 'vue-event-calendar';
+Vue.use(vueEventCalendar, { locale: "en", color: "#C41230" });
 
 Vue.config.productionTip = false
 
